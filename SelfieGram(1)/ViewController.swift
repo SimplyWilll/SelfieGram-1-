7de2bdc1f94016d3639e741954a5de73b4fd6024
SimplyWilll <william970306@hotmail.com>
2016-05-24 18:52:14 -0700

    Initial Commit

---
+++ SelfieGram(1)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfieGram(1)
+//
+//  Created by William Jiang on 2016-05-24.
+//  Copyright © 2016 William Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SelfieGram(1)Tests/SelfieGram_1_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SelfieGram_1_Tests.swift
+//  SelfieGram(1)Tests
+//
+//  Created by William Jiang on 2016-05-24.
+//  Copyright © 2016 William Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import SelfieGram_1_
+
+class SelfieGram_1_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
